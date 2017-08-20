0d5eb7d031d1f647df10c68aad62196eaa0c8d85
Trevor Pellegrini <trevorpellegrini@gmail.com>
2017-08-20 15:23:22 -0700

    added sagas and redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+'use strict';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'react-redux';
+import configureStore from './store/configure-store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = configureStore()
+
+ReactDOM.render(
+    <Provider store={store}>
+    <App/>
+</Provider>, document.getElementById('root'));
 registerServiceWorker();
